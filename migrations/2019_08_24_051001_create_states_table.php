d8d21909cd03edad20e61e77d9b663d5150e26b8
Seddiq Sorush <seddiq.sorush@gmail.com>
2020-04-18 12:23:45 +0430

    initial commit

---
+++ migrations/2019_08_24_051001_create_states_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name');
+            $table->unsignedInteger('country_id');      
+        });
+        Schema::table('states', function(Blueprint $table){
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
---
+++ migrations/2019_08_24_051009_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id')->index();
+            $table->string('name');
+            $table->unsignedInteger('state_id');   
+        });
+        Schema::table('cities', function(Blueprint $table){
+            $table->foreign('state_id')->references('id')->on('states');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
